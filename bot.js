761550f0b558d3edcc3b76332f9e752f889c2fd8
rishabhratnalikar <75599765+rishabhratnalikar@users.noreply.github.com>
2021-02-09 10:35:32 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,31 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
 client.on('ready', () =>{
   
-  console.log("!Chanchlani Bot is online")
+  console.log("Chanchlani Bot is online")
   
 })
+
 client.on('msg', msg=>{
   
   
-  if(msg.content == "-taarif")
-  msg.reply('https://tenor.com/view/ashish-chanchlani-waah-waah-taarif-kar-raha-he-gif-18773741')
+ if(msg.content === "taarif"){
+    msg.reply('https://tenor.com/view/ashish-chanchlani-waah-waah-taarif-kar-raha-he-gif-18773741')
+ }
   
-  if(msg.content == "-gussa")
-  msg.reply('https://tenor.com/view/mishtersami-ashish-ashishchinchilani-itnamaruga-gif-19828548')
+ if(msg.content === "gussa"){
+      msg.reply('https://tenor.com/view/mishtersami-ashish-ashishchinchilani-itnamaruga-gif-19828548')
+ }
   
-  if(msg.content == "-mazza")
-  msg.reply('https://tenor.com/view/mishtersami-ashish-ashishchinchilani-birijalily-dingdong-gif-19828524')
+ if(msg.content === "mazza"){
+    msg.reply('https://tenor.com/view/mishtersami-ashish-ashishchinchilani-birijalily-dingdong-gif-19828524')
+ }
   
-  if(msg.content == "-gn")
-  msg.reply('https://tenor.com/view/mishtersami-ashish-ashishchinchilani-goodmorning-goodnight-gif-19828592')
+ if(msg.content === "gn"){
+    msg.reply('https://tenor.com/view/mishtersami-ashish-ashishchinchilani-goodmorning-goodnight-gif-19828592')
 }
           
 })
-
 client.login(process.env.BOT_TOKEN);
